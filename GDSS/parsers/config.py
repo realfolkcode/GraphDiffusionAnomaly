cefdd3dbfe77da42a7dbc27330d78274c4f902bd
realfolkcode <wtfomgrofl@mail.ru>
2022-12-09 13:47:17 +0300

    Change GDSS get_config argument to path

--- GDSS/parsers/config.py
+++ GDSS/parsers/config.py
@@ -2,9 +2,8 @@ import yaml
 from easydict import EasyDict as edict
 
 
-def get_config(config, seed):
-    config_dir = f'./config/{config}.yaml'
-    config = edict(yaml.load(open(config_dir, 'r'), Loader=yaml.FullLoader))
+def get_config(config_path, seed):
+    config = edict(yaml.load(open(config_path, 'r'), Loader=yaml.FullLoader))
     config.seed = seed
 
     return config
