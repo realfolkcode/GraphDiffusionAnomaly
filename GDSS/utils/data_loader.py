13fef09f13ca5a193ad3d3325db7349073a03f93
realfolkcode <wtfomgrofl@mail.ru>
2022-12-10 12:52:39 +0300

    Move sampling into GDSS and add dataloader

--- GDSS/utils/data_loader.py
+++ GDSS/utils/data_loader.py
@@ -1,23 +1,48 @@
-from torch.utils.data import TensorDataset, DataLoader
-from data.data_generators import load_dataset
-from utils.graph_utils import init_features, graphs_to_tensor
+import torch
+from torch.utils.data.sampler import SubsetRandomSampler
+import torch.nn.functional as F
+import dgl
+from dgl.dataloading import GraphDataLoader
+import numpy as np
+from functools import partial
+
+from .graph_utils import pad_adjs
 
 
-def graphs_to_dataloader(config, graph_list):
+def sample_subgraph(g, max_node_num):
+    if len(g.nodes()) <= max_node_num:
+        return g
+    idx = []
+    idx += np.random.choice(np.arange(1, len(g.nodes())), 
+                            max_node_num,
+                            replace=False).tolist()
+    return dgl.node_subgraph(g, idx)
 
-    adjs_tensor = graphs_to_tensor(graph_list, config.data.max_node_num)
-    x_tensor = init_features(config.data.init, adjs_tensor, config.data.max_feat_num)
 
-    train_ds = TensorDataset(x_tensor, adjs_tensor)
-    train_dl = DataLoader(train_ds, batch_size=config.data.batch_size, shuffle=True)
-    return train_dl
+def collate_fn(graphs, max_node_num):
+    graphs = [sample_subgraph(g, max_node_num) for g in graphs]
+
+    graph_list = [g.adj().to_dense() for g in graphs]
+
+    graph_list = [pad_adjs(g, max_node_num) for g in graph_list]
+    adjs_tensor = torch.stack(graph_list)
+
+    x_tensor = torch.stack([F.pad(g.ndata['x'], 
+                                  (0, 0, 0, max(max_node_num - len(g.nodes()), 0)), 
+                                  "constant", 0)
+                            for g in graphs])
+
+    return x_tensor, adjs_tensor
 
 
-def dataloader(config, get_graph_list=False):
-    graph_list = load_dataset(data_dir=config.data.dir, file_name=config.data.data)
-    test_size = int(config.data.test_split * len(graph_list))
-    train_graph_list, test_graph_list = graph_list[test_size:], graph_list[:test_size]
-    if get_graph_list:
-        return train_graph_list, test_graph_list
-
-    return graphs_to_dataloader(config, train_graph_list), graphs_to_dataloader(config, test_graph_list)
+def dataloader(config, dataset):
+    train_idx = dataset.graph.nodes()
+    train_sampler = SubsetRandomSampler(train_idx)
+    loader = GraphDataLoader(
+        dataset,
+        sampler=train_sampler,
+        batch_size=128,
+        drop_last=True,
+        collate_fn=partial(collate_fn, max_node_num=config.data.max_node_num)
+    )
+    return loader
