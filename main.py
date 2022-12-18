691249000c3da84b6fbd569a7af956123a2e74f6
realfolkcode <wtfomgrofl@mail.ru>
2022-12-18 14:12:36 +0300

    Add GDSS training

--- main.py
+++ main.py
@@ -19,19 +19,29 @@ from data import AnomalyDataset
 def main(args):
     config = get_config(args.config, args.seed)
 
+    # Load dataset
     dataset_name = config.data.data
     dataset = AnomalyDataset(dataset_name, radius=1)
     print(f'Dataset: {dataset_name}')
     print(f'Number of nodes: {len(dataset)}')
 
+    # Adjust max node num
     ego_lens = dataset.graph.out_degrees().numpy()
     max_node_num = int(np.quantile(ego_lens, 0.95))
     print(f'Max size subgraphs (95% quantile): {max_node_num}')
 
+    # Train GDSS
+    trainer = Trainer(config)
+    train_loader = dataloader(config, dataset)
+    trainer.train_loader = train_loader
+    ckpt = trainer.train(args.exp_name)
+    config.ckpt = ckpt
+
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, required=True)
-    parser.add_argument('--seed', type=int, default=42, required=False)
+    parser.add_argument('--config', type=str, required=True, help='config path')
+    parser.add_argument('--exp_name', type=str, required=True, help='experiment name')
+    parser.add_argument('--seed', type=int, default=42, required=False, help='rng seed value')
     args = parser.parse_args()
     main(args)
