a0903e85bee4648357a714fdf21ac979896b3fc2
realfolkcode <wtfomgrofl@mail.ru>
2022-12-30 19:05:21 +0900

    Rework subgraph sampling

--- GDSS/utils/data_loader.py
+++ GDSS/utils/data_loader.py
@@ -12,10 +12,14 @@ from .graph_utils import pad_adjs
 def sample_subgraph(g, max_node_num):
     if len(g.nodes()) <= max_node_num:
         return g
-    idx = [0]
-    idx += np.random.choice(np.arange(1, len(g.nodes())), 
-                            max_node_num - 1,
-                            replace=False).tolist()
+    idx = []
+    for bfs_nodes in dgl.bfs_nodes_generator(g, 0):
+        remaining = max_node_num - len(idx)
+        if remaining <= 0:
+            break
+        k = min(remaining, len(bfs_nodes))
+        idx += bfs_nodes[:k]
+    idx = idx[:max_node_num]
     return dgl.node_subgraph(g, idx)
 
 
