5ce495de34afe6cfcc50ef181b896fcde326def3
realfolkcode <wtfomgrofl@mail.ru>
2023-01-19 12:09:43 +0300

    Pass eig flag to eig score model

--- GSDM/utils/loader.py
+++ GSDM/utils/loader.py
@@ -167,7 +167,7 @@ def load_model_params(config):
     params_adj = {'model_type': config_m.x, 'max_feat_num': max_feat_num, 'depth': config_m.depth, 
                 'nhid': config_m.nhid, 'num_linears': config_m.num_linears,
                 'c_init': config_m.c_init, 'c_hid': config_m.c_hid, 'c_final': config_m.c_final, 
-                'adim': config_m.adim, 'num_heads': config_m.num_heads, 'conv':config_m.conv}
+                'adim': config_m.adim, 'num_heads': config_m.num_heads, 'conv':config_m.conv, 'is_eig':True}
     return params_x, params_adj
 
 
