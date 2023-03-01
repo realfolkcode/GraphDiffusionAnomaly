Fix bug with prior likelihood calculation

GDSS/likelihood.py
@@ -7,7 +7,7 @@ from GDSS.utils.loader import load_device, load_seed, load_model_from_ckpt, \
                               load_ema_from_ckpt, load_ckpt, load_sampling_fn, \
                               load_batch, load_sde
 from GDSS.utils.graph_utils import node_flags, gen_noise, mask_x, mask_adjs, \
-                                   quantize
+                                   quantize, count_nodes
 
 
 def get_div_fn(fn_x, fn_adj):
@@ -94,7 +94,9 @@ def get_likelihood_fn(sde_x, sde_adj,
       z_x = from_flattened_numpy(zp[:len_flat_x], shape_x).to(x.device).type(torch.float32)
       z_adj = from_flattened_numpy(zp[len_flat_x:-bs], shape_adj).to(adj.device).type(torch.float32)
       delta_logp = from_flattened_numpy(zp[-bs:], (bs,)).to(x.device).type(torch.float32)
-      prior_logp = sde_x.prior_logp(z_x) + sde_adj.prior_logp(z_adj)
+      N_x = count_nodes(adj) * shape_x[-1]
+      N_adj = count_nodes(adj)**2
+      prior_logp = sde_x.prior_logp(z_x, N_x) + sde_adj.prior_logp(z_adj, N_adj)
       nll = -(prior_logp + delta_logp)
       return nll
 

GDSS/sde.py
@@ -37,11 +37,12 @@ class SDE(abc.ABC):
     pass
 
   @abc.abstractmethod
-  def prior_logp(self, z):
+  def prior_logp(self, z, N):
     """Compute log-density of the prior distribution.
     Useful for computing the log-likelihood via probability flow ODE.
     Args:
       z: latent code
+      N: number of components
     Returns:
       log probability density
     """
@@ -146,9 +147,7 @@ class VPSDE(SDE):
     x = torch.randn(*shape).triu(1) 
     return x + x.transpose(-1,-2)
 
-  def prior_logp(self, z):
-    shape = z.shape
-    N = np.prod(shape[1:])
+  def prior_logp(self, z, N):
     logps = -N / 2. * np.log(2 * np.pi) - torch.sum(z ** 2, dim=(1, 2)) / 2.
     return logps
 
@@ -208,9 +207,7 @@ class VESDE(SDE):
     x = x + x.transpose(-1,-2)
     return x 
 
-  def prior_logp(self, z):
-    shape = z.shape
-    N = np.prod(shape[1:])
+  def prior_logp(self, z, N):
     return -N / 2. * np.log(2 * np.pi * self.sigma_max ** 2) - torch.sum(z ** 2, dim=(1, 2, 3)) / (2 * self.sigma_max ** 2)
 
   def discretize(self, x, t):
@@ -271,7 +268,5 @@ class subVPSDE(SDE):
     x = torch.randn(*shape).triu(1) 
     return x + x.transpose(-1,-2)
 
-  def prior_logp(self, z):
-    shape = z.shape
-    N = np.prod(shape[1:])
+  def prior_logp(self, z, N):
     return -N / 2. * np.log(2 * np.pi) - torch.sum(z ** 2, dim=(1, 2, 3)) / 2.