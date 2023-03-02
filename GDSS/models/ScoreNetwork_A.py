60e2dff1ce702c30d6f69a7be4846eeb6a8ea63b
realfolkcode <wtfomgrofl@mail.ru>
2023-03-02 10:56:27 +0300

    Symmetrize score adj after MLP

--- GDSS/models/ScoreNetwork_A.py
+++ GDSS/models/ScoreNetwork_A.py
@@ -101,7 +101,7 @@ class BaselineNetwork(torch.nn.Module):
 class ScoreNetworkA(BaselineNetwork):
 
     def __init__(self, max_feat_num, max_node_num, nhid, num_layers, num_linears, 
-                    c_init, c_hid, c_final, adim, num_heads=4, conv='GCN'):
+                    c_init, c_hid, c_final, adim, num_heads=4, conv='GCN', sym=True):
 
         super(ScoreNetworkA, self).__init__(max_feat_num, max_node_num, nhid, num_layers, num_linears, 
                                             c_init, c_hid, c_final, adim, num_heads=4, conv='GCN')
@@ -109,6 +109,7 @@ class ScoreNetworkA(BaselineNetwork):
         self.adim = adim
         self.num_heads = num_heads
         self.conv = conv
+        self.sym = sym
 
         self.layers = torch.nn.ModuleList()
         self.layers.append(AttentionLayer(self.num_linears, self.nfeat, self.nhid, self.nhid, self.c_init, 
@@ -136,6 +137,9 @@ class ScoreNetworkA(BaselineNetwork):
         
         self.mask = self.mask.to(score.device)
         score = score * self.mask
+
+        if self.sym:
+            score = score + score.transpose(-1, -2)
 
         score = mask_adjs(score, flags)
 
