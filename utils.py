569f43d0db587d0731beaba2e846d83f4febb711
realfolkcode <wtfomgrofl@mail.ru>
2023-03-23 12:13:26 +0300

    Add SNR calculation function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 def standardize(x, eps=1e-7):
@@ -6,3 +7,16 @@ def standardize(x, eps=1e-7):
     s = x.std(0, unbiased=False, keepdim=True)
     x = (x - m) / (s + eps)
     return x
+
+
+def calculate_snr(T_lst, config_sde, eps=1e-7):
+    sde_type = config_sde.type
+    if sde_type == 'VP':
+        beta_min = config_sde.beta_min
+        beta_max = config_sde.beta_max
+        integral = beta_min * T_lst - (beta_max - beta_min) / 2 * T_lst**2
+        mu_squared = np.exp(integral)
+        sigma_squared = 1 - np.exp(integral)
+        return mu_squared / (sigma_squared + eps)
+    else:
+        raise NotImplementedError(f'{sde_type} not implemented.')
